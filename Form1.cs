c7b472451bbdb8178b899878966d832309e3ef3f
Rhuan Santos <rhuansantosdev@gmail.com>
2016-07-26 22:59:32 -0400

    Add database connection

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient; // using mysql
 
 namespace leaderTest
 {
     public partial class Form1 : Form
     {
+
+
+        // global variaables
+        private string conn;
+        private MySqlConnection connect;
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        // function for the database connect
+        private void db_connection()
+        {
+            try
+            {
+                conn = "server=127.0.0.1;userid=root;password=root;database=windows-form; port=3306;";
+                connect = new MySqlConnection(conn);
+                connect.Open();
+            }
+            catch (MySqlException e)
+            {
+                throw;
+            }
+        }
+
+
+
+
     }
 }
